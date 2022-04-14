fb4c1c4e44714406fba8598edd5848e645d8d7a8
meir-poccin <83933438+meir-poccin@users.noreply.github.com>
2022-04-13 20:32:36 -0400

    first commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () =>{
+	return(
+    <div>
+    <img alt='' src='https://robohash.org/test'/>
+    <div>
+    	<h2>joek</h2>
+    	<p>joe@gmail.com</p>
+    </div>
+    </div>
+		);
+}
+export default Card;
